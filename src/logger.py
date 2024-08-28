15493e691f1153e8f8bf02fa227b406329a73da7
Nick <nickgabriadze@gmail.com>
2024-08-28 21:00:43 +0400

    fix: remove server id from main(moved to logger.py file)

--- src/logger.py
+++ src/logger.py
@@ -1,9 +1,12 @@
 import logging
+from os import getenv
 
 
 def setupLogging():
+    SERVER_ID = getenv("SERVER_ID", "1")
+
     logging.basicConfig(level=logging.INFO,
-                        format='[DATE] %(asctime)s - [%(levelname)s] - %(message)s',
+                        format=f'[DATE] %(asctime)s - [SERVER ID] {SERVER_ID} - [%(levelname)s] - %(message)s',
                         datefmt="%Y-%m-%d %H:%M:%S",
                         filename='fastapi.log')
 
