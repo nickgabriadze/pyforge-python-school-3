2e742923ec174ae9b0ac7b78211f550dd7f1580f
Nick <nickgabriadze@gmail.com>
2024-08-28 21:35:30 +0400

    refactor: update format

--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,7 @@ def setupLogging():
     SERVER_ID = getenv("SERVER_ID", "1")
 
     logging.basicConfig(level=logging.INFO,
-                        format=f'[DATE] %(asctime)s - [SERVER ID] {SERVER_ID} - [%(levelname)s] - %(message)s',
+                        format=f'[DATE] %(asctime)s - [SERVER] {SERVER_ID} - [%(levelname)s] - %(message)s',
                         datefmt="%Y-%m-%d %H:%M:%S",
                         filename='fastapi.log')
 
