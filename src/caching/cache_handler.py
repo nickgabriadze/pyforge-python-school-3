27a0a74e3356fd4900fd3a2cd40469415ab2cf70
Nick <nickgabriadze@gmail.com>
2024-09-01 18:04:58 +0400

    feat: add caching functionality

---
+++ src/caching/cache_handler.py
@@ -0,0 +1,21 @@
+import json
+import redis
+
+
+def get_cached_result(client, key: str):
+    result = client.get(key)
+
+    if result:
+        return json.loads(result)
+
+    return None
+
+
+def remove_cache(client, key:str):
+    cacheExists = get_cached_result(client, key)
+    if cacheExists:
+        redis_client.delete(key)
+
+def set_cache(client, key: str, value: dict, exp: int = 60):
+    client.setex(key, exp, json.dumps(value))
+
