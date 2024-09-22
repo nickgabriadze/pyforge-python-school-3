9b2e5b3e8fbe6ae04fee72766954c1b68e3b9dba
Nick <nickgabriadze@gmail.com>
2024-09-22 18:00:44 +0400

    feat: add function to test lambda's functionality

---
+++ src/invoke_lambda.py
@@ -0,0 +1,13 @@
+import boto3
+
+lambda_client = boto3.client('lambda')
+
+response = lambda_client.invoke(
+    FunctionName='HelloStudentFunction',
+    InvocationType='RequestResponse',
+    Payload=b'{"names": ["Nick", "Bob", "Mike"]}'         
+)
+
+response_payload = response['Payload'].read()
+print(response_payload)
+
