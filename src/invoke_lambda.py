8294fbdfdce621cea7e8f3e39b3d84cbf38420b5
Nick <nickgabriadze@gmail.com>
2024-09-22 18:05:11 +0400

    refactor: format file

--- src/invoke_lambda.py
+++ src/invoke_lambda.py
@@ -5,7 +5,7 @@ lambda_client = boto3.client('lambda')
 response = lambda_client.invoke(
     FunctionName='HelloStudentFunction',
     InvocationType='RequestResponse',
-    Payload=b'{"names": ["Nick", "Bob", "Mike"]}'         
+    Payload=b'{"names": ["Nick", "Bob", "Mike"]}'
 )
 
 response_payload = response['Payload'].read()
